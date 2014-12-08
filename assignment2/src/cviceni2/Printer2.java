c0a6802551859d79fb77f7c2cdf3f6f54da2cb76
kljueva <kljueva@gaia.ms.mff.cuni.cz>
2014-12-08 10:57:01 +0100

    commit assignment2 - first draft

---
+++ assignment2/src/cviceni2/Printer2.java
@@ -0,0 +1,10 @@
+package cviceni2;
+
+public class Printer2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Testing printing out");
+	}
+
+}
